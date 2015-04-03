a4d724a92d6b9d8b45fde786557576081fcf6e1c
roy <roy_radio@126.com>
2015-04-04 07:17:20 +0800

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by 刘迪 on 4/4/15.
+//  Copyright (c) 2015 roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
