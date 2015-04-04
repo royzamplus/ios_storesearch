8c19a79f8c7a137a33d24e40c23f2a3c90e9f9fd
roy <roy_radio@126.com>
2015-04-04 08:18:57 +0800

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by 刘迪 on 4/4/15.
+//  Copyright (c) 2015 roy. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
